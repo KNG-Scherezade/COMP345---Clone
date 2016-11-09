a862a00d78051c100ab604618d5d598b80be6748
ConnorYoung <c_oung@encs.concordia.ca>
2016-11-08 21:12:46 -0500

    Add files via upload

---
+++ Map.cpp
@@ -0,0 +1,342 @@
+#include "Map.h"
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+Map::Map()
+{
+	rows = 2;
+	columns = 2;
+	initializeMap();
+}
+
+Map::Map(Map * copyMap)
+{
+	rows = copyMap->getRows();
+	columns = copyMap->getColumns();
+	mapName = copyMap->getMapName();
+	// Initialize rows
+	map = new char*[rows];
+
+	// Initialize pointers to columns
+	for (int i = 0; i < rows; ++i) {
+		map[i] = new char[columns];
+	}
+
+	// Make a copy of the map recieved as a param.
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			map[i][j] = copyMap->getCharObject(i,j);
+		}
+	}
+
+}
+
+Map::Map(int newRows, int newColumns) : rows(newRows), columns(newColumns)
+{
+	initializeMap();
+}
+
+Map::Map(int newRows, int newColumns, string newName) : rows(newRows), columns(newColumns), mapName(newName)
+{
+	initializeMap();
+}
+
+Map::~Map()
+{
+	deleteMap();
+}
+
+bool Map::validatePath()
+{
+	Node* startPointer = findStart();
+	if (startPointer != 0) {
+		Node* endPointer = findEnd();
+		if (endPointer != 0) {
+			bool result = findValidPath(startPointer, endPointer);
+
+			// Delete pointers that will no longer be used
+			delete startPointer;
+			delete endPointer;
+			startPointer = 0;
+			endPointer = 0;
+
+			return result;
+		}
+		else {
+			cout << "Map is invalid. Cannot find end cell.\n";
+
+			// Delete pointer that will no longer be used
+			delete startPointer;
+			startPointer = 0;
+
+			return false;
+		}
+	}
+	else {
+		cout << "Map is invalid. Cannot find start cell.\n";
+		return false;
+	}
+} 
+
+void Map::fillCell(int row, int column, char obj)
+{
+	// If the object is a start or end cell, make sure that one does not already exist.
+	if (obj == 's' || obj == 'e') {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (map[i][j] == obj) {
+					cout << "Cannot have more than one start or end cell.";
+					return;
+				}
+			}
+		}
+	}
+	map[row][column] = obj;
+}
+
+bool Map::isOccupied(int row, int column)
+{
+	return (map[row][column] != ' ');
+}
+
+void Map::printMap()
+{
+	cout << mapName << " :\n";
+	for (int i = 0; i < rows; i++) {
+		cout << "\n[ ";
+		for (int j = 0; j < columns; j++) {
+			cout << map[i][j] << " ";
+		}
+		cout << "]";
+	}
+	cout << "\n";
+}
+
+void Map::setMapName(string newName)
+{
+	mapName = newName;
+}
+
+int Map::getRows()
+{
+	return rows;
+}
+
+int Map::getColumns()
+{
+	return columns;
+}
+
+string Map::getMapName()
+{
+	return mapName;
+}
+
+char Map::getCharObject(int row, int column)
+{
+	if (row < rows && row >= 0) {
+		if (column < columns && column >= 0) {
+			return map[row][column];
+		}
+		else {
+			cout << "Invalid column index\n";
+			return 0;
+		}
+	}
+	else {
+		cout << "Invalid row index\n";
+		return 0;
+	}
+}
+
+void Map::initializeMap()
+{
+	// Initialize rows
+	map = new char*[rows];
+
+	// Initialize pointers to columns
+	for (int i = 0; i < rows; ++i) {
+		map[i] = new char[columns];
+	}
+
+	// Set default empty value for all the elemts in the map matrix
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			map[i][j] = ' ';
+		}
+	}
+}
+
+void Map::deleteMap() {
+	// Delete pointers to columns
+	for (int i = 0; i < rows; ++i) {
+		delete map[i];
+	}
+
+	// Delete rows
+	delete map;
+}
+
+Node* Map::findStart()
+{
+	Node* startPointer = 0;
+	int nodeIdIndex = 0;
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			if (map[i][j] == 's') {
+				startPointer = new Node (nodeIdIndex, i, j);
+				startPointer->setTraversable(true);
+			}
+			nodeIdIndex++;
+		}
+	}
+	return startPointer;
+}
+
+Node* Map::findEnd()
+{
+	Node* endPointer = 0;
+	int nodeIdIndex = 0;
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			if (map[i][j] == 'e') {
+				endPointer = new Node (nodeIdIndex, i, j, 0, true);
+			}
+			nodeIdIndex++;
+		}
+	}
+	return endPointer;
+}
+
+bool Map::findValidPath(Node* startPointer, Node* endPointer)
+{
+	int id;
+	int currentRow;
+	int currentColumn;
+	int heuristic;
+	int const maxHeuristic = rows + columns;
+
+	unordered_map<int, Node> parentMap;
+	unordered_map<int, Node> closedMap;
+
+	int const endNodeRow = endPointer->getRow();
+	int const endNodeColumn = endPointer->getColumn();
+
+	currentRow = startPointer->getRow();
+	currentColumn = startPointer->getColumn();
+
+	id = calculateNodeId(currentRow, currentColumn);
+	heuristic = calculateHeuristic(currentRow, currentColumn, endNodeRow, endNodeColumn);
+
+	startPointer->setHeuristic(heuristic);
+	startPointer->setId(id);
+	parentMap[id] = *startPointer;
+	do {
+
+		// Check if the current heuristic is 1, meaning that a valid path was found
+		if (heuristic == 1) {
+			return true;
+		}
+
+		// Remove current node from parentMap and add it to closedMap
+		closedMap[id] = parentMap[id];
+		parentMap.erase(id);
+
+		currentRow = closedMap[id].getRow();
+		currentColumn = closedMap[id].getColumn();
+
+		// The following code is very similar in all four if statements. Only the row or the column changes.
+		// Ideally, it should be placed within a private method and sent the row and column. However, I would then
+		// need to figure out an efficient way to send the parentMap without wasting resources. One way to do this
+		// would be to use a pointer but I decided not to because I thought it would waste too many resources in this case.
+
+		// Check the top node
+		int tempRow = currentRow - 1;
+		if (tempRow > -1) {
+			id = calculateNodeId(tempRow, currentColumn);
+
+			// Check if the node was already found.
+			bool notFound = (parentMap.find(id) == parentMap.end() && closedMap.find(id) == closedMap.end());
+			if (notFound) {
+				bool traversable = (map[tempRow][currentColumn] == ' ');
+				if (traversable) {
+					heuristic = calculateHeuristic(tempRow, currentColumn, endNodeRow, endNodeColumn);
+					parentMap[id] = Node(id, tempRow, currentColumn, heuristic, traversable);
+				}
+			}
+		}
+
+		// Check the bottom node
+		tempRow = currentRow + 1;
+		if (tempRow < rows) {
+			id = calculateNodeId(tempRow, currentColumn);
+
+			// Check if the node was already found.
+			bool notFound = (parentMap.find(id) == parentMap.end() && closedMap.find(id) == closedMap.end());
+			if (notFound) {
+				bool traversable = (map[tempRow][currentColumn] == ' ');
+				if (traversable) {
+					heuristic = calculateHeuristic(tempRow, currentColumn, endNodeRow, endNodeColumn);
+					parentMap[id] = Node(id, tempRow, currentColumn, heuristic, traversable);
+				}
+			}
+		}
+
+		// Check the left node
+		int tempColumn = currentColumn - 1;
+		if (tempColumn > -1) {
+			id = calculateNodeId(currentRow, tempColumn);
+
+			// Check if the node was already found.
+			bool notFound = (parentMap.find(id) == parentMap.end() && closedMap.find(id) == closedMap.end());
+			if (notFound) {
+				bool traversable = (map[currentRow][tempColumn] == ' ');
+				if (traversable) {
+					heuristic = calculateHeuristic(currentRow, tempColumn, endNodeRow, endNodeColumn);
+					parentMap[id] = Node(id, currentRow, tempColumn, heuristic, traversable);
+				}
+			}
+		}
+
+		// Check the right node
+		tempColumn = currentColumn + 1;
+		if (tempColumn < columns) {
+			id = calculateNodeId(currentRow, tempColumn);
+
+			// Check if the node was already found.
+			bool notFound = (parentMap.find(id) == parentMap.end() && closedMap.find(id) == closedMap.end());
+			if (notFound) {
+				bool traversable = (map[currentRow][tempColumn] == ' ');
+				if (traversable) {
+					heuristic = calculateHeuristic(currentRow, tempColumn, endNodeRow, endNodeColumn);
+					parentMap[id] = Node(id, currentRow, tempColumn, heuristic, traversable);
+				}
+			}
+		}
+
+		heuristic = maxHeuristic;
+
+		// Get details of parent with smallest heuristic
+		for (auto &node : parentMap) {
+			if (node.second.getHeuristic() <= heuristic) {
+				heuristic = node.second.getHeuristic();
+				id = node.first;
+			}
+		}
+	} while (parentMap.size() != 0);
+
+	cout << "Map is invalid. No valid path between the start and end cells exists.\n";
+	return false;
+}
+
+int Map::calculateNodeId(int row, int column)
+{
+	return (rows * row + column);
+}
+
+int Map::calculateHeuristic(int row, int column, int endRow, int endColumn)
+{
+	return (abs(endRow - row) + abs(endColumn - column));
+}
---
+++ Map.h
@@ -0,0 +1,148 @@
+#pragma once
+#include "Node.h"
+#include <vector>
+using namespace std;
+
+//! @file 
+//! @brief		Class representing the map. Will most likely change in future versions
+//!
+
+//! Class representing the map.
+
+//! Game rules:	This class allows for the creation of customs maps of variable size that will be determined
+//!				prior to generation of the map. The map will be able to contain traversable empty cells, 
+//!				untraversable walls, and cells that will be occupied by the character, enemies, chests, doors, 
+//!				and possibly more. The class also allows to objects to be inserted into cells,
+//!				to check if a certain cell contains an object, and to validate the map	by veryfing that it
+//!				contains a start cell, and end cell, and that there is a path of empty cells between them.
+//!		
+//! Design:		No external libraries have been used. The map is a char matrix. Currently, the empty char ' '
+//!				represents an empty space in the map, the char 'e' represents the end cell, and the char 's'
+//!				represents the start cell, the char "|" represents a wall, the char "t" represents a chest,
+//!				the char "m" represents a monster. At the moment, any other character will indicate that the cell is not
+//!				traversable. In the future, enums may be implemented to force specific values into the map instead of
+//!				stand alone characters.
+class Map
+{
+public:
+	Map();
+	Map(Map* copyMap);
+	Map(int newRows, int newColumns);
+	Map(int newRows, int newColumns, string newName);
+	~Map();
+
+	//! Searches for the start cell represented by te character 's'. 
+	//!
+	//! @return	Node pointer representing the start cell, if found. 
+	//!			Else, returns an empty pointer.
+	Node* findStart();
+
+	//! Searches for the end cell represented by te character 'e'. 
+	//!
+	//! @return	Node pointer representing the end cell, if found. 
+	//!			Else, returns an empty pointer.
+	Node* findEnd();
+
+	//! General method for validating the path. First calls a methods that verifiy that the map has a start
+	//! and an end (findStart and findEnd). Then calls another method to check if a valid path exists
+	//! within the map (findValidPath).
+	//!
+	//! @return	bool indicating whether or not the map has a valid path the start and end cells.
+	bool validatePath();
+
+	//! Fills the specified cell with a char representing an object.
+	//! The isOccupied function should be called in the controller, before calling this function, 
+	//! to make sure that the cell is empty.
+	//!
+	//! @param	row		Row where the char will be inserted.
+	//!	@param	column	Column where the char will be inserted.
+	//! @param	obj		Character that will be inserted.
+	void fillCell(int row, int column, char obj);
+
+	//! Returns a bool value indicating whether or not the specified cell is occcupied.
+	//!
+	//! @param	row		Row that needs to be checked.
+	//! @param	column	Column that needs to be checked.	
+	//!
+	//! @return			bool indicating whether or not the specified cell is occupied.
+	bool isOccupied(int row, int column);
+
+	//! Prints the map with its contents.
+	void printMap();
+
+	//! Sets the map's name
+	//! @param	newName		New name to set for the map.
+	void setMapName(string newName);
+
+	//! Returns the number of rows in the map.
+	//!	@return	the number of rows in the map
+	int getRows();
+
+	//! Returns the number of columns in the map.
+	//!	@return	the number of columns in the map
+	int getColumns();
+	
+	//! Returns the map name.
+	//!	@return	the map name.
+	string getMapName();
+
+	//! Returns the cha saved at a position in the map.
+	//!
+	//! @param	row		Row that needs to be checked.
+	//! @param	column	Column that needs to be checked.	
+	//!
+	//! @return	char in the row and column sent as parameters
+	char getCharObject(int row, int column);
+	
+private:
+	//! Number of rows in the map.
+	int rows;
+
+	//! Number of columns in the map.
+	int columns;
+
+	//! Char representation of the map.
+	char **map;
+
+	//! Used as filename when saving maps. If maps have the same name, the file will be overwritten.
+	string mapName;
+
+	//! Allocates memory for the map and sets the cell to empty values.
+	//! Empty cells are represented by an empty space character (' ').
+	void initializeMap();
+
+	//! Dealocates memory for the map.
+	void deleteMap();
+
+	//! Searches for a valid path within the map. Returns a bool value indicating whether or not
+	//! a valid path exists between the start and end cells. Personal implementation of the a* algorithm.
+	//! Does not currently take keys and doors into account.
+	//! 
+	//! @param	startPointer	Pointer to the Node representing the start cell.
+	//! @param	endPointer		Pointer to the Node representing the end cell.
+	//!
+	//! @return	bool value indicating whether or not a valid path exists between the start and end cells.
+	bool findValidPath(Node* startPointer, Node* endPointer);
+
+	//! Calculates the id for a Node. The id of a Node is it's linear position in the map.
+	//! In other words, it's the x-th cell the map.
+	//!
+	//! @param		row		The row the Node is located on.
+	//!	@param		column	The column the Node is located on.
+	//!
+	//!	@return		The node's id.
+	int calculateNodeId(int row, int column);
+
+	//! Calculates the heuristic for a Node. The heuristic is the number of cells 
+	//! the Node is away from the end cell.
+	//!
+	//! @param		row			The row the Node is located on.
+	//!	@param		column		The column the Node is located on.
+	//!	@param		endRow		The row the end cell is located on.
+	//!	@param		endColumn	The column the end cell is located on.
+	//!
+	//!	@return		The Node's heuristic value. The heuristic is the number of cells 
+	//!				the Node is away from the end cell.
+	int calculateHeuristic(int row, int column, int endRow, int endColumn);
+};
+
